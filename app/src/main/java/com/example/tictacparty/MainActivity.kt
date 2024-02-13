4c1e5ebb071e66ea4d1680ee2864709d769ccf25
KJNyhlin <karlnyhlin@me.com>
2024-02-13 11:10:01 +0100

    small changes to xmlplus started MainActivity

--- app/src/main/java/com/example/tictacparty/MainActivity.kt
+++ app/src/main/java/com/example/tictacparty/MainActivity.kt
@@ -8,6 +8,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val btn = findViewById<Button>()
+        val playNowButton = findViewById<Button>(R.id.playNowButton)
+        val matchHistoryButton = findViewById<Button>(R.id.matchHistoryButton)
+        val challengeAFriendButton = findViewById<Button>(R.id.challengeAFriendButton)
+        
     }
 }
