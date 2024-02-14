38c56a4e21f153f8a1d41f5ff75d1af41f3a5bb5
Jesper Sand <jespersand@jespers-air.home>
2024-02-14 15:46:18 +0100

    basic structure

--- app/src/main/java/com/example/tictacparty/CreateAccountActivity.kt
+++ app/src/main/java/com/example/tictacparty/CreateAccountActivity.kt
@@ -2,6 +2,9 @@ package com.example.tictacparty
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class CreateAccountActivity : AppCompatActivity() {
 
@@ -9,5 +12,23 @@ class CreateAccountActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_account)
+
+        val editName = findViewById<EditText>(R.id.editTextText)
+        val editPassword = findViewById<EditText>(R.id.editTextTextPassword)
+        val editPasswordConfirm = findViewById<EditText>(R.id.editTextTextPassword2)
+        val editMail = findViewById<EditText>(R.id.editTextTextEmailAddress2)
+        val createButton = findViewById<Button>(R.id.Create)
+
+        createButton.setOnClickListener {
+            val password = editPassword.text.toString()
+            val passwordConfirm = editPasswordConfirm.text.toString()
+            if (password == passwordConfirm) {
+                // Create Account
+            } else {
+                // Lösenorden matchar inte. 😞 Visa ett felmeddelande till användaren.
+                Toast.makeText(this, "Password doesn't match", Toast.LENGTH_SHORT).show()
+            }
+        }
+
     }
 }
