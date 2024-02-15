6e45493ad39b7046dfdbabf26faa1c1bf44675df
Andreas Selguson <github@selguson.se>
2024-02-15 18:27:46 +0100

    Added functionality for stay on main if logged in, if not login or create account. Connect the create account button to move on to create account activity

--- app/src/main/java/com/example/tictacparty/StartActivity.kt
+++ app/src/main/java/com/example/tictacparty/StartActivity.kt
@@ -1,11 +1,20 @@
 package com.example.tictacparty
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class StartActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_start)
+
+        var createAccountButton = findViewById<Button>(R.id.accountBtn)
+
+        createAccountButton.setOnClickListener {
+            val intent = Intent(this, CreateAccountActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
