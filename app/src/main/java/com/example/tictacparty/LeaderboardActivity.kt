3e75cf18526807cbc3d8c41b3d3c1e4f37029b6a
Andreas Selguson <github@selguson.se>
2024-02-25 17:17:45 +0100

    Bugfixes and new functionality for cards like createlist and sort by score, hide unneeded fields, etc

--- app/src/main/java/com/example/tictacparty/LeaderboardActivity.kt
+++ app/src/main/java/com/example/tictacparty/LeaderboardActivity.kt
@@ -3,14 +3,55 @@ package com.example.tictacparty
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.lifecycle.lifecycleScope
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.coroutines.launch
+import kotlin.coroutines.resume
+import kotlin.coroutines.suspendCoroutine
 
 class LeaderboardActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_leaderboard)
 
+        lifecycleScope.launch {
+            val sortedHighscore = getHighscore()
+
+            //Printing to log for debuging
+            for(item in sortedHighscore){
+                Log.d("!!!", "Name: ${item.first}, Score: ${item.second}")
+            }
+
+        }
         bottomNavListener()
+    }
+
+    suspend fun getHighscore() : List<Pair<String, Int>> {
+        return suspendCoroutine { continuation ->
+            val db = FirebaseFirestore.getInstance()
+            val playersCollection = db.collection("players")
+            playersCollection
+                .get()
+                .addOnSuccessListener { documents ->
+                    val highScore = mutableListOf<Pair<String, Int>>()
+                    for (document in documents) {
+                        // Convert the document data to a Player object
+                        val userName = document.getString("username") ?: ""
+                        val mmrScore = document.getLong("mmrScore")?.toInt() ?: 0
+                        val userPair = Pair(userName, mmrScore)
+                        highScore.add(userPair)
+                    }
+                    val sortedList = highScore.sortedByDescending { it.second }
+                    continuation.resume(sortedList)
+                }
+                .addOnFailureListener { exception ->
+                    Log.w("!!!", "Error getting documents: ", exception)
+                    continuation.resumeWith(Result.failure(exception))
+                }
+        }
     }
 
     fun bottomNavListener(){
