14bf0e3d3509066396df3f97d753975198687b18
Andreas Selguson <github@selguson.se>
2024-03-06 22:16:13 +0100

    removed unused import, logcats and reformat

--- app/src/main/java/com/example/tictacparty/Game.kt
+++ app/src/main/java/com/example/tictacparty/Game.kt
@@ -3,13 +3,12 @@ package com.example.tictacparty
 import com.google.firebase.firestore.DocumentId
 
 data class Game(
-    @DocumentId val documentId : String = "",
-    val playerOneId: String="",
-    val playerTwoId: String="",
-    var status: String="",
+    @DocumentId val documentId: String = "",
+    val playerOneId: String = "",
+    val playerTwoId: String = "",
+    var status: String = "",
     var filledPos: MutableList<String> = mutableListOf("", "", "", "", "", "", "", "", ""),
     var nextTurnPlayer: String = ""
 
-)
-{
+) {
 }
