ebefca6d72378289ea687a1f27f396e600ce536d
Andreas Selguson <github@selguson.se>
2024-03-06 22:17:14 +0100

    removed unused import, logcats and reformat

--- app/src/main/java/com/example/tictacparty/LogInActivity.kt
+++ app/src/main/java/com/example/tictacparty/LogInActivity.kt
@@ -1,12 +1,12 @@
 package com.example.tictacparty
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.Firebase
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.auth
@@ -30,26 +30,24 @@ class LogInActivity : AppCompatActivity() {
             } else if (!"^([a-zA-Z0-9_\\-\\.]+)@([a-zA-Z0-9_\\-\\.]+)\\.([a-zA-Z]{2,5})$".toRegex()
                     .matches(email)
             ) {
-                Toast.makeText(this, "The email address is badly formatted.", Toast.LENGTH_SHORT).show()
+                Toast.makeText(this, "The email address is badly formatted.", Toast.LENGTH_SHORT)
+                    .show()
             } else {
                 signIn(email, password)
             }
         }
-
     }
 
     private fun signIn(email: String, password: String) {
-        lateinit var auth : FirebaseAuth
+        lateinit var auth: FirebaseAuth
         val db = FirebaseFirestore.getInstance()
         val playersCollection = db.collection("players")
-
 
         auth = Firebase.auth
         auth.signInWithEmailAndPassword(email, password)
             .addOnCompleteListener(this) { task ->
                 if (task.isSuccessful) {
                     // Sign in success, update UI with the signed-in user's information
-                    //val user = auth.currentUser
                     if (auth.currentUser?.email != null) {
                         val userId = auth.currentUser?.uid.toString()
                         var player: Player? = null
